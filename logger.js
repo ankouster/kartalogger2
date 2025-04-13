b76dde1e9bd663ea159b4fcbb0b5fb663c685c32
TheAnkouster <adamoleon22@gmail.com>
2025-04-13 03:56:16 -0300

    Delete logger.js

--- logger.js
+++
@@ -1,8 +0,0 @@
-app.post('/webhook', (req, res) => {
-    const data = req.body;
-    const userAgent = data.userAgent;
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log('IP del usuario:', ip);
-    console.log('User Agent:', userAgent);
-    res.status(200).send('Datos recibidos');
-});
